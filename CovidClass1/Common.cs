72ad13d3d9b832929682a9ef4ac573d563b9b89e
Pascal Ojinnaka <ojinnnakapascal@gmail.com>
2020-04-27 18:31:37 +0100

    CovidClass 1

---
+++ CovidClass1/Common.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CovidClass1
+{
+    public class Common
+    {
+        
+        public string Name { get; set; }
+        public string Location { get; set; }
+        public string Category { get; set; }
+        public string Gender { get; set; }
+        public string DisplayGender { get; set; }
+    }
+    public class Mentors
+    {
+        public List<Mentor> MentorsList = new List<Mentor>();
+    }
+
+    public class Mentor : Common
+    {
+        public int YearsOfExperience { get; set; }
+    }
+}
---
+++ CovidClass1/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CovidClass1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Mentor mentor1 = new Mentor() {
+                Name = "Pascal Ojinnaka",
+                Location = "Canada",
+                Gender = "Male",
+                YearsOfExperience = 4,
+                Category = "Programming"
+            };
+            Mentor mentor2 = new Mentor()
+            {
+                Name = "Stanley Ojinnaka",
+                Location = "Ghana",
+                Gender = "Male",
+                YearsOfExperience = 5,
+                Category = "Data Analyst"
+            };
+            Mentor mentor3 = new Mentor()
+            {
+                Name = "Amanda Ojinnaka",
+                Location = "USA",
+                Gender = "Female",
+                YearsOfExperience = 5,
+                Category = "Cloud Analyst"
+            };
+
+            Mentors listOfMentors = new Mentors();
+
+            listOfMentors.MentorsList.Add(mentor1);
+            listOfMentors.MentorsList.Add(mentor2);
+            listOfMentors.MentorsList.Add(mentor3);
+
+            foreach (Mentor m in listOfMentors.MentorsList)
+            {
+                Mentor y = GetGender(m);
+                Console.WriteLine($"Our Mentor is {y.Name}, {y.DisplayGender} " +
+                    $"lives in {y.Location} and has {y.YearsOfExperience} years of experience in {y.Category} ");
+            }
+
+        }
+
+        private static Mentor GetGender(Mentor m)
+        {
+            if (m.Gender == "Male")
+            {
+                m.DisplayGender = "He";
+            }
+            else
+            {
+                m.DisplayGender = "She";
+            }
+            return m;
+        }
+
+    }
+}
